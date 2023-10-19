e636c460ba8e66060cb77cb763ce99e65aac79cb
Nathaniel Varona <nathanielvarona@users.noreply.github.com>
2023-10-19 13:51:55 +0800

    Profile Server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,52 +1,42 @@
 #!/bin/sh
 
-# Get the directory of the currently executing script
-SCRIPT_PATH="$GITHUB_ACTION_PATH$([[ "$RUNNER_OS" == "Windows" ]] && echo '\\' || echo '/')entrypoint.sh"
+PRITUNL_PROFILE_FILE="${PRITUNL_PROFILE_FILE:-}"
+PRITUNL_PROFILE_SERVER="${PRITUNL_PROFILE_SERVER:-}"
 
-# Print the full path of the script directory
-echo "The full path of the script is: $SCRIPT_PATH"
+# Access the profile-server input as a JSON-encoded string
+profile_server_input="$PRITUNL_PROFILE_SERVER"
 
+echo "###"
+echo "Passing Inputs..."
+echo "####"
+echo $profile_server_input
+echo "#####"
 
-# Get the full path of the script file
-SCRIPT_PATH2=$(realpath "$0")
+# Check if the input is JSON-encoded, a single string, or a YAML array
+if [[ "$profile_server_input" == \[* ]]; then
+  # Input is already a Bash array (YAML format)
+  echo "Yaml List, in multi line"
+  server_names=("${profile_server_input[@]}")
+elif [[ "$profile_server_input" == *,* ]]; then
+  echo "Yaml List, in single line"
+  # Input is a comma-separated string, convert to a Bash array
+  IFS=',' read -ra server_names <<< "$profile_server_input"
+else
+  echo "Single entry, in string"
+  # Input is a single string, create a Bash array with one element
+  server_names=("$profile_server_input")
+fi
 
-# Print the full path of the script file
-echo "The full path 2 of the script is: $SCRIPT_PATH2"
+echo "###"
+echo "Server Names in Array"
+echo "####"
+echo $server_names
+echo "#####"
 
-# # Access the profile-server input as a JSON-encoded string
-# profile_server_input="${{ inputs.profile-server }}"
-
-# echo "###"
-# echo "Passing Inputs..."
-# echo "####"
-# echo $profile_server_input
-# echo "#####"
-
-# # Check if the input is JSON-encoded, a single string, or a YAML array
-# if [[ "$profile_server_input" == \[* ]]; then
-#   # Input is already a Bash array (YAML format)
-#   echo "Yaml List, in multi line"
-#   server_names=("${profile_server_input[@]}")
-# elif [[ "$profile_server_input" == *,* ]]; then
-#   echo "Yaml List, in single line"
-#   # Input is a comma-separated string, convert to a Bash array
-#   IFS=',' read -ra server_names <<< "$profile_server_input"
-# else
-#   echo "Single entry, in string"
-#   # Input is a single string, create a Bash array with one element
-#   server_names=("$profile_server_input")
-# fi
-
-# echo "###"
-# echo "Server Names in Array"
-# echo "####"
-# echo $server_names
-# echo "#####"
-
-# # Loop through the server names
-# for server_name in "${server_names[@]}"
-# do
-#   # Use the server name in your script
-#   echo "Connecting to server: $server_name"
-#   # Add your Pritunl connection logic here
-# done
+# Loop through the server names
+for server_name in "${server_names[@]}"
+do
+  # Use the server name in your script
+  echo "Connecting to server: $server_name"
+  # Add your Pritunl connection logic here
+done
