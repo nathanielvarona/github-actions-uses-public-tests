79d48b3e8d42cec8c0e57e6d2fb884aae9de5bd4
Nathaniel Varona <nathanielvarona@users.noreply.github.com>
2023-10-19 21:42:18 +0800

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,14 @@
 
 which bash
 
-PRITUNL_PROFILE_SERVER="quality-assurance, sales-marketing, research-development"
-# PRITUNL_PROFILE_SERVER="quality-assurance"
+# PRITUNL_PROFILE_SERVER="quality-assurance, sales-marketing, research-development"
 
+# PRITUNL_PROFILE_SERVER="quality-assurance"
 # PRITUNL_PROFILE_FILE="${PRITUNL_PROFILE_FILE:-}"
+
 PRITUNL_PROFILE_SERVER="${PRITUNL_PROFILE_SERVER:-}"
 
-echo $PRITUNL_PROFILE_SERVER
-
-profile_server=($(echo "$PRITUNL_PROFILE_SERVER" | tr ', ' ' '))
-
-# IFS=', ' read -r -a profile_server <<< "$PRITUNL_PROFILE_SERVER"
-
-echo $profile_server
+IFS=', ' read -r -a profile_server <<< "$PRITUNL_PROFILE_SERVER"
 
 echo '####'
 
